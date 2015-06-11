f98f6a90588127304c29e3ce13f55c86d855bfe8
nicoleroden <nicole.roden@gmail.com>
2015-06-11 11:18:13 -0500

    latest

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ActiveRecord::Base
 
 validates :name,:presence => true
-#validates :purchased, :presence => true
+validates :purchased, :presence => true
 validates :expiration,:presence => true
-#validates :location, :presence => true
+validates :link, :presence => true
 validates :room_id, :presence => true
 
 
