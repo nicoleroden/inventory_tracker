86097add15aba4b9749d07dc7d03c7f05ae050bd
nicoleroden <nicole.roden@gmail.com>
2015-06-11 09:24:51 -0500

    morning updates

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ActiveRecord::Base
 
-  validates :name, :presence =>true, :uniqueness => {:scope => :user_id}
+  validates :name, :presence =>true, :uniqueness => {:scope => :user_id, case_sensitive: false}
 
-  has_many :items
+  has_many :items, dependent: :destroy
   belongs_to :user
 
 end
